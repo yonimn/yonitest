088a387fd4c6a2dfad7283c746cb47a47a79a7a9
Jonathan Menache <yonimn@gmail.com>
2013-04-23 01:31:11 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yonitest::Application.config.secret_token = '133128fec2d821dd2dfc821daca0678282dbc8c1d3b817b06a86321866fbe381a070dea149c4c1b6f31f0af0cdbbe9db2a80fae04f7e99f15f8f7e85b737e9fa'
