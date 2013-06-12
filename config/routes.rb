e74cae10e33df4e40b392151155e4bf5cb7ecbd4
Jonathan Menache <yonimn@gmail.com>
2013-06-12 19:39:28 +0300

    add bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Yonitest::Application.routes.draw do
+ get 'about' => 'pages#about'
+
+
+
+
+  root :to => 'pages#home'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
